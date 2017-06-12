b114e11ec40bcb81a7b9586a80014aee33a87353
Valentin Vetter <valentin@codingame.com>
2017-06-12 10:18:34 +0200

    cleanup: Use 2 spaces instead of tabs

--- nodejs-project/universe.js
+++ nodejs-project/universe.js
@@ -1,9 +1,9 @@
 function countAllStars(galaxies) {
-	var totalStars = 0;
-	galaxies.forEach(function(stars) {
-		totalStars = stars;
-	}, this);
-	return totalStars;
+  var totalStars = 0;
+  galaxies.forEach(function(stars) {
+    totalStars = stars;
+  }, this);
+  return totalStars;
 }
 
 module.exports = countAllStars;
